742a8ca5530fd1faf3e7b5de9039f4b2a5c8c0c1
Ramtrap <Ramtrap@aol.com>
2017-08-03 21:51:37 -0400

    Edits.

--- ex12.py
+++ ex12.py
@@ -4,7 +4,7 @@ age = raw_input("How old are  you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
 
-print "So, you're %r old, %r tall and %r lbs heavy." % (
+print "So, you're %r old, %r tall and %r heavy." % (
     age, height, weight)
 
 print "To read how functions work, type: \"pydoc raw_input\", for example"
