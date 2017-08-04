aa62538737439fce3a9c0413e2aa5ec606aa859b
Will <Ramtrap@aol.com>
2017-08-03 21:02:49 -0400

    Completed Exercise 11.

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "Raw Input = So, you're %r years old, %r tall and weigh %r lbs." % (age, height, weight)
+print "User View = So, you're %s years old, %s tall and weigh %s lbs." % (age, height, weight)
